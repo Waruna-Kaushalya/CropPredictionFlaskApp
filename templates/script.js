753ace63d6cdb321b5bb073d711126fc809faf93
Waruna-Kaushalya <waruna95610@gmail.com>
2020-10-15 14:05:37 +0530

    Add validation and some changes

---
+++ templates/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    $('#containerForm').bootstrapValidator({
+      container: 'tooltip',
+      feedbackIcons: {
+        valid: 'glyphicon glyphicon-ok',
+        invalid: 'glyphicon glyphicon-remove',
+        validating: 'glyphicon glyphicon-refresh'
+      },
+      fields: {
+
+        Rainfall: {
+          validators: {
+            numeric: {
+              message: 'Please enter numeric values only'
+            },
+            notEmpty: {
+              message: '* Field is required'
+            }
+          }
+        },
+
+        MaximumTemperature: {
+          validators: {
+            numeric: {
+              message: 'Please enter numeric values only'
+            },
+            notEmpty: {
+              message: '* Field is required'
+            }
+          }
+        },
+
+        MinimumTemperature: {
+          validators: {
+            numeric: {
+              message: 'Please enter numeric values only'
+            },
+            notEmpty: {
+              message: '* Field is required'
+            }
+          }
+        },
+
+        RelativeHumidity: {
+          validators: {
+            numeric: {
+              message: 'Please enter numeric values only'
+            },
+            notEmpty: {
+              message: '* Field is required'
+            }
+          }
+        },
+
+        Pressure: {
+          validators: {
+            numeric: {
+              message: 'Please enter numeric values only'
+            },
+            notEmpty: {
+              message: '* Field is required'
+            }
+          }
+        }
+
+        
+      }
+    });
+  });
