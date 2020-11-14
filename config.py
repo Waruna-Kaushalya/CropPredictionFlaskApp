a54b9676ee06aae4e271cad4870cfe08a2694b38
Waruna-Kaushalya <waruna95610@gmail.com>
2020-11-14 19:07:31 +0530

    Commit aws env variable

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
+import sys
+import boto3
 
 # AWS configurations
-S3_BUCKET                 = "flask-s3-crop"
-S3_KEY                    = "AKIA4YEQCWGSAZKWZFLF"
-S3_SECRET                 = "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"
+
+session = boto3.Session()
+
+credentials = session.get_credentials()
+S3_KEY = credentials.access_key
+S3_SECRET = credentials.secret_key
+S3_BUCKET = "flask-s3-crop"
