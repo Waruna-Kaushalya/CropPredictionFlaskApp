19d08160da2a0bdf721592d7f33e48e0ed00a596
Waruna-Kaushalya <waruna95610@gmail.com>
2020-11-14 19:18:28 +0530

    aws config

--- config.py
+++ config.py
@@ -4,9 +4,11 @@ import boto3
 
 # AWS configurations
 
-session = boto3.Session()
-
-credentials = session.get_credentials()
-S3_KEY = credentials.access_key
-S3_SECRET = credentials.secret_key
 S3_BUCKET = "flask-s3-crop"
+S3_KEY                    = "AKIA4YEQCWGSDZANPYOB"
+S3_SECRET                 = "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"
+
+# session = boto3.Session()
+# credentials = session.get_credentials()
+# S3_KEY = credentials.access_key
+# S3_SECRET = credentials.secret_key
