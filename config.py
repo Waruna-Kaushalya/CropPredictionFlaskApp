d0f12b267d23ce5a1c8005c0869fded05ea94d00
Waruna-Kaushalya <waruna95610@gmail.com>
2020-11-16 10:45:04 +0530

    Test heroku env variables

--- config.py
+++ config.py
@@ -5,10 +5,17 @@ import boto3
 # AWS configurations
 
 S3_BUCKET = "flask-s3-crop"
-S3_KEY                    = "AKIA4YEQCWGSDZANPYOB"
-S3_SECRET                 = "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"
+# S3_KEY                    = "AKIA4YEQCWGSDZANPYOB"
+# S3_SECRET                 = "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"
 
 # session = boto3.Session()
 # credentials = session.get_credentials()
 # S3_KEY = credentials.access_key
 # S3_SECRET = credentials.secret_key
+
+from boto.s3.connection import S3Connection
+# s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+
+S3_KEY = os.environ['S3_KEY']
+S3_SECRET = os.environ['S3_SECRET']
+
